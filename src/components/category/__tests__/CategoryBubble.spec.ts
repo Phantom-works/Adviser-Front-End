90e4a0814135456bdadc398482c6d339e1ebdf2d
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-05-08 10:03:58 +0200

    🧪 Putting our component to the test! Added tests for the category select component and told Jest to hush up about window.alert 🤫🚨🔍

--- src/components/category/__tests__/CategoryBubble.spec.ts
+++ src/components/category/__tests__/CategoryBubble.spec.ts
@@ -5,44 +5,9 @@ import CategoryBubble from '../CategoryBubble.vue'
 describe('CategoryBubble Snapshots', () => {
   // Arrange
 
-  it('Snapshot text empty ', () => {
+  it('Snapshot Bubble Default', () => {
     //Arrange
-    const category = {
-      id: 0,
-      title: ''
-    }
-    // Act
-    const wrapper = mount(CategoryBubble, {
-      props: {
-        category: category
-      }
-    })
-    // Assert
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('Snapshot text long ', () => {
-    //Arrange
-    const category = {
-      id: 0,
-      title: 'zerdy8y;ljsdrty88;reasdrty;yaetjsdl'
-    }
-    // Act
-    const wrapper = mount(CategoryBubble, {
-      props: {
-        category: category
-      }
-    })
-    // Assert
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('Snapshot text small ', () => {
-    //Arrange
-    const category = {
-      id: 0,
-      title: 'hallo'
-    }
+    const category = {}
     // Act
     const wrapper = mount(CategoryBubble, {
       props: {
